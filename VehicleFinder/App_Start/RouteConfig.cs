a3b9f8a4a125a78ea1ebdd70aa727770e77c4da1
Mark Griep <mgriep@summit-mortgage.com>
2019-01-17 10:48:02 -0600

    Added notes on how the routing works and the patterns it follows

--- VehicleFinder/App_Start/RouteConfig.cs
+++ VehicleFinder/App_Start/RouteConfig.cs
@@ -18,6 +18,19 @@ namespace VehicleFinder
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            // Here are the routing rules 
+            // Follows this pattern NameOfController, ActionName, ID to pass to the action
+            // 
+            // If it's https://www.foo.com/movies/edit/1  it will go to   MoviesController.Edit(1)
+            
+
+            // Defaults work this way: If url is missing parts...
+            // If it's https://www.foo.com/               it will go to  HomeController.Index()    -- neither controller nor action is specified
+            // If it's https://www.foo.com/movies         it will go to  MoviesController.Index()  -- controller is specified, but not action
+            // If it's https://www.foo.com/movies/rand    it will go to  MoviesController.Rand()   -- all are specified
+
+            // Notice ID is optional. Not all actions take in an ID.
         }
     }
 }
