8dc246a53f4e554ed2da0cf5b8a52dcca8df8462
Mark Griep <mgriep@summit-mortgage.com>
2019-01-17 13:40:54 -0600

    Changed to use a new Bootstrap theme. Changed site.css to bring navbar to top changed Layout to have a correct navbar

--- VehicleFinder/App_Start/BundleConfig.cs
+++ VehicleFinder/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace VehicleFinder
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-darkly.css",
                       "~/Content/site.css"));
         }
     }
