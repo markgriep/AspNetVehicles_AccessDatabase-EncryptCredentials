531b763bd53de85899914ba5665b3ccf8e639c82
Mark Griep <mgriep@summit-mortgage.com>
2019-01-17 11:06:42 -0600

    Notes about global.asax

--- VehicleFinder/Global.asax.cs
+++ VehicleFinder/Global.asax.cs
@@ -14,8 +14,23 @@ namespace VehicleFinder
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);   // these are routes for our application
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+
+        //  Global ASX is one of the traditional files that has been in a split up net for a long time and it's
+        //  a class that provides hooks for various events in the applications lifecycle.
+        //  So let's expand this and open the C-sharp file.
+        //  So when the application is started this method will be called.
+        //  And here you can see we're registering a few things like the routes.
+        //  So when the application started we tell the runtime.
+        //  These are the routes for our application.
+
+
+
+
+
+
     }
 }
