b4d8ac865a9ba9f06e1e6429edf6237eb02f0867
Mark Griep <mgriep@summit-mortgage.com>
2019-01-17 11:38:51 -0600

    Added a new Model, View and Controller for vehicles.

---
+++ VehicleFinder/Controllers/VehicleController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VehicleFinder.Models;
+
+namespace VehicleFinder.Controllers
+{
+    public class VehicleController : Controller
+    {
+        // GET: Vehicle
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Special()
+        {
+            var car = new Vehicle()
+            { VehicleId = 1001,
+                VehicleMake = "Toyota",
+                VehicleModel = "Sienna",
+                VehicleYear = 2006
+            };
+            return View(car);
+        }
+
+
+    }
+}
---
+++ VehicleFinder/Models/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleFinder.Models
+{
+    public class Vehicle
+    {
+        public int VehicleId { get; set; }
+        public string VehicleMake { get; set; }
+        public string VehicleModel { get; set; }
+        public int VehicleYear { get; set; }
+    }
+}
+
